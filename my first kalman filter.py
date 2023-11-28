407a80ac55712f851d31403941bb57e0c637ea70
Henrick Ku <36804898+hkkhkhkhk@users.noreply.github.com>
2023-11-28 20:23:14 +0000

    init

---
+++ my first kalman filter.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+Estimation_error = 0 # dynamic and looped
+Measurement_error = 4 #static, you can decide yourself for the filter performance
+
+Model_error = 1/80 # this is how much our model guesses wrong, inject some constant to make it drift
+#you shouldn't be able to change the model error in real life as it is limited by how rubbish your model is
+
+def Kalman_gain(Eest, Emea):
+    return Eest / (Eest + Emea)
+
+def state_estimator(current, step_num):
+    if step_num == 0:
+        return 0
+    return current + time_step*np.cos(x_values[step_num]) + Model_error # introduce some consistent error to simulate integration error of a model
+
+def corrected_estimate(prev_state_estimate, measurement, k):
+    return prev_state_estimate + k*(measurement - prev_state_estimate)
+
+def update_estimation_error(prev_estimation_error, k):
+    return 1 - k * prev_estimation_error
+
+# Generate data using a loop
+num_steps = 100
+x_values = np.linspace(0, 2 * np.pi, num_steps)
+time_step = 2 * np.pi/num_steps
+
+true_sine_wave = np.zeros(num_steps)
+noisy_sine_wave = np.zeros(num_steps)
+state_prediction = np.zeros(num_steps)
+corrected_state = np.zeros(num_steps)
+
+
+for i in range(num_steps):
+    true_sine_wave[i] = np.sin(x_values[i])
+    noisy_sine_wave[i] = true_sine_wave[i] + np.random.normal(0, 0.1)
+
+    K = Kalman_gain(Estimation_error, Measurement_error)
+    # State prediction that is not corrected, will go into the cosmos
+    state_prediction[i] = state_estimator(state_prediction[i-1], i)
+    
+    #state prediction correced by the noisy current signal
+    corrected_state[i] = corrected_estimate(state_estimator(corrected_state[i-1], i), noisy_sine_wave[i], K)
+
+    #lastly lets update the estimation error
+    Estimation_error = update_estimation_error(Estimation_error, K)
+    
+    
+
+# Plot the sine wave
+plt.plot(x_values, true_sine_wave, label='True Sine Wave', color='blue')
+
+# Plot the noisy sine wave
+plt.plot(x_values, noisy_sine_wave, label='Noisy Sine Wave', color='red')
+
+# Plot the state prediction
+plt.plot(x_values, state_prediction, label='State Prediction (Raw)', color='green', linestyle='dashed')
+
+plt.plot(x_values, corrected_state, label='State Prediction after Kalman (Corrected)', color='black')
+
+# Add labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('True Sine Wave, Noisy Measurement, and State Prediction')
+
+plt.rcParams['figure.dpi'] = 800
+# Add a legend
+plt.legend()
+
+# Show the plot
+plt.show()
