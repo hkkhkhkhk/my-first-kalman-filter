59804deaf0ef9055715313e4cf637d127455cf89
Henrick Ku <36804898+hkkhkhkhk@users.noreply.github.com>
2023-11-28 23:53:31 +0000

    Update my first kalman filter.py

--- my first kalman filter.py
+++ my first kalman filter.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 Estimation_error = 0 # dynamic and looped
-Measurement_error = 4 #static, you can decide yourself for the filter performance
+Measurement_variance = 100 #static, how confident you are in your sensors (in n*Var)
+Proccess_noise_variance = 3 #Static, how confident you are in your model (in n*Var)
 
 Model_error = 1/80 # this is how much our model guesses wrong, inject some constant to make it drift
 #you shouldn't be able to change the model error in real life as it is limited by how rubbish your model is
@@ -11,15 +12,13 @@ def Kalman_gain(Eest, Emea):
     return Eest / (Eest + Emea)
 
 def state_estimator(current, step_num):
-    if step_num == 0:
-        return 0
     return current + time_step*np.cos(x_values[step_num]) + Model_error # introduce some consistent error to simulate integration error of a model
 
 def corrected_estimate(prev_state_estimate, measurement, k):
     return prev_state_estimate + k*(measurement - prev_state_estimate)
 
 def update_estimation_error(prev_estimation_error, k):
-    return 1 - k * prev_estimation_error
+    return (1 - k) * prev_estimation_error + Proccess_noise_variance
 
 # Generate data using a loop
 num_steps = 100
@@ -36,7 +35,7 @@ for i in range(num_steps):
     true_sine_wave[i] = np.sin(x_values[i])
     noisy_sine_wave[i] = true_sine_wave[i] + np.random.normal(0, 0.1)
 
-    K = Kalman_gain(Estimation_error, Measurement_error)
+    K = Kalman_gain(Estimation_error, Measurement_variance)
     # State prediction that is not corrected, will go into the cosmos
     state_prediction[i] = state_estimator(state_prediction[i-1], i)
     
@@ -45,8 +44,6 @@ for i in range(num_steps):
 
     #lastly lets update the estimation error
     Estimation_error = update_estimation_error(Estimation_error, K)
-    
-    
 
 # Plot the sine wave
 plt.plot(x_values, true_sine_wave, label='True Sine Wave', color='blue')
